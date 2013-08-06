c57cb5862f3e366b3bf22004ffd5a14b07006fde
green-creeper <andrey.korlyuk@gmail.com>
2013-08-06 18:58:48 +0300

    Added commandline agrument check

--- src/main/java/JavaWaveform/App.java
+++ src/main/java/JavaWaveform/App.java
@@ -5,14 +5,22 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.*;
 
-public class App 
-{
-    public static void main( String[] args ) throws UnsupportedAudioFileException, IOException {
+public class App {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Usage \n JavaWaveform.jar [filename]");
+            return;
+        }
         File file = new File(args[0]);
-        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+        AudioInputStream audioInputStream = null;
+        try {
+            audioInputStream = AudioSystem.getAudioInputStream(file);
+        } catch (IOException | UnsupportedAudioFileException e) {
+            System.out.println("couldn't open file");
+            e.printStackTrace();
+        }
         int frameLength = (int) audioInputStream.getFrameLength();
-        int frameSize = (int) audioInputStream.getFormat().getFrameSize();
-        int numChannels = audioInputStream.getFormat().getChannels();
+        int frameSize = audioInputStream.getFormat().getFrameSize();
         int[] toReturn = new int[frameLength];
         byte[] bytes = new byte[frameLength * frameSize];
 
@@ -25,13 +33,13 @@ public class App
 
         int sampleIndex = 0;
 
-        for (int t = 0; t < bytes.length;) {
-                int low = (int) bytes[t];
-                t++;
-                int high = (int) bytes[t];
-                t++;
-                int sample = getSixteenBitSample(high, low);
-                toReturn[sampleIndex] = sample;
+        for (int t = 0; t < bytes.length; ) {
+            int low = (int) bytes[t];
+            t++;
+            int high = (int) bytes[t];
+            t++;
+            int sample = getSixteenBitSample(high, low);
+            toReturn[sampleIndex] = sample;
             sampleIndex++;
         }
 
@@ -41,16 +49,20 @@ public class App
             writer = new BufferedWriter(new OutputStreamWriter(
                     new FileOutputStream("data.txt"), "utf-8"));
             writer.write("[");
-            for (int i = 0; i < toReturn.length; i+=(toReturn.length/1500)) {
-                writer.write(Double.valueOf(toReturn[i])/4000 + ",");
+            for (int i = 0; i < toReturn.length; i += (toReturn.length / 1500)) {
+                writer.write(Double.valueOf(toReturn[i]) / 4000 + ",");
             }
             writer.write("]");
-        } catch (IOException ex){
+        } catch (IOException ex) {
             // report
         } finally {
-            try {writer.close();} catch (Exception ex) {}
+            try {
+                writer.close();
+            } catch (Exception ex) {
+            }
         }
     }
+
     private static int getSixteenBitSample(int high, int low) {
         return (high << 8) + (low & 0x00ff);
     }
